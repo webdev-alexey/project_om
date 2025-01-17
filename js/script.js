7a36cb0ff42d96645faa8ce674b235970cc64d11
alexey <alexey.anasov@yandex.by>
2025-01-17 19:04:45 +0300

    добавлен slider-blog

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 $(function () {
   // Mixitup
   var mixer = mixitup(".portfolio__content");
+
+  // slider
+  $(".slider-blog__inner").slick({
+    dots: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow-right"></button>',
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
 });
