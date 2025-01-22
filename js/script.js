f85492fbbd9958360619ae012f08794c10a2da87
alexey <alexey.anasov@yandex.by>
2025-01-22 17:30:44 +0300

    блок connection

--- js/script.js
+++ js/script.js
@@ -18,4 +18,22 @@ $(function () {
       },
     ],
   });
+
+  // форма
+  $(".footer__form").submit(function () {
+    //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "php/mail.php", //Change
+      data: th.serialize(),
+    }).done(function () {
+      alert("Спасибо! Скоро свяжемся!");
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
 });
